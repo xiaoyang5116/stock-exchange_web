30a478a508befefc69369e288744b430baf689be
shawn <1443584075@qq.com>
2023-12-17 18:12:10 +0800

    feat: 添加基础库和内嵌tradingview

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 import Nav from "./components/Nav";
+import EmbeddedStock from "./components/EmbeddedStock";
 
 function App() {
   return (
@@ -17,24 +18,29 @@ function App() {
         sm: "70px 1fr",
       }}
       minH={"100vh"}
-      gap='2'
+      gap="2"
       bg={"#FAFAFA"}
     >
-      <GridItem area={'header'}>
-        <Text color={"#241717"} mt={{
-          base: "0",
-          sm: "15px"
-        }}>Header</Text>
+      <GridItem area={"header"}>
+        <Text
+          color={"#241717"}
+          mt={{
+            base: "0",
+            sm: "15px",
+          }}
+        >
+          Header
+        </Text>
       </GridItem>
       <Show above={"sm"}>
-        <GridItem bg='#fff' area={'nav'}>
+        <GridItem bg="#fff" area={"nav"}>
           <Nav />
         </GridItem>
       </Show>
-      <GridItem bg='#fff' area={'main'} borderRadius="5px">
-        <Text color={"#000"}>Main</Text>
+      <GridItem bg="#fff" area={"main"} borderRadius="5px">
+        <EmbeddedStock />
       </GridItem>
-    </Grid >
+    </Grid>
   );
 }
 
